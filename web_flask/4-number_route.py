7ed87833e75fd2861af7030ca593bd99711dcf0d
root <lizosazi@gmail.com>
2024-03-16 20:36:56 +0000

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+starts flask web app and listens on port 0.0.0.0 and 5000
+/c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+The default value of text is “is cool"
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Index():
+    """Prints Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Prints HBNB!"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIsCool(text="is cool"):
+    """prints pyhon_is_cool on the screen"""
+    return 'Python ' + text.replace('_',' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """returns 98 is a number is n is a number"""
+    return '{:d} is a number'.format(n)
+
+
+
+
+if __name__ == "__main__":
+    app.run( host="0.0.0.0", port='5000')
