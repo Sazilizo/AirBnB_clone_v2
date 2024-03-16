7344728421dd4ccb54d9d12aa919c99794251938
root <lizosazi@gmail.com>
2024-03-16 18:48:16 +0000

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+ #!/usr/bin/python3
+"""starts flask web app and listens on port 0.0.0.0 and 5000 """
+import flask
+
+app =flask.Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hello():
+  """Prints Hello HBNB!"""
+  return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+  app.run( host="0.0.0.0", port='5000')
